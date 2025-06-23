62b0c70613257cb5242446184ab87af55e29610c
Noah12398 <149400947+Noah12398@users.noreply.github.com>
2025-06-23 19:05:04 +0530

    Update3

--- my-portfolio/vite.config.js
+++ my-portfolio/vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' // if you're using React
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()], // include your existing plugins
-  base: '/Port_Folio/', // Replace 'my-portfolio' with your actual repository name
+  plugins: [react()],
+  base: '/Port_Folio/', // Replace with your actual GitHub repo name
   build: {
     outDir: 'dist'
   }
-})
+});
